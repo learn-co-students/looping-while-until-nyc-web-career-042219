8268a565017c429335c1bb2a1f21a6bbfd05970b
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-04 04:22:34 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,7 @@
-
-counter = 6
 def using_until
+  counter = 6
   until counter == 10
-  puts "Wingardium Leviosa"
   counter += 1
+  puts "Wingardium Leviosa"
+ end 
 end
--- lib/while.rb
+++ lib/while.rb
@@ -1,5 +1,10 @@
 
 def using_while
+  i = 6
+  while i < 10
+  i += 1
+  puts "Wingardium Leviosa"
+end
 end
 
 
